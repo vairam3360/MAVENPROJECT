ac800133f0a5677e48fd25ad3c632f7285c45165
vairam3360 <vairamuthu.mit@gmail.com>
2019-01-27 01:03:23 -0500

    First commit

---
+++ src/test/java/testNGPackage/testClassNew.java
@@ -0,0 +1,25 @@
+package testNGPackage;//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+
+public class testClassNew {
+
+
+
+
+        @Test
+        public void testNumber1(){
+            System.out.println("Testing Number 1");
+
+        }
+
+        @BeforeEach
+        public void testNumber2(){
+            System.out.println("Testing Number 2 new line new commit");
+
+        }
+
+    }
+
